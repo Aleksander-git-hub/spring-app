d0100e102728f7bba13343eafff1b1288c993005
Aleksander-git-hub <aleksander.chugunoff@yandex.ru>
2021-01-21 00:45:03 +0300

    DI Part 2 Setter Reference

--- src/main/java/com/petproject/TestSpring.java
+++ src/main/java/com/petproject/TestSpring.java
@@ -9,7 +9,7 @@ public class TestSpring
                 "applicationContext.xml"
         );
 
-       /* Music musicBean = context.getBean("musicBean", Music.class);
+       /*Music musicBean = context.getBean("musicBean", Music.class);
         MusicPlayer musicPlayer = new MusicPlayer(musicBean);*/
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
